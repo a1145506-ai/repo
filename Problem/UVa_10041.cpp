57165c438fcf636ff85d5d01fbcb9aa39b187211
a1145506-ai <a1145506@mail.nuk.edu.tw>
2026-05-26 06:09:15 +0000

    Vito's family

--- Problem/UVa_10041.cpp
+++ Problem/UVa_10041.cpp
@@ -1,29 +1,40 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int T;
-    cin >> T;
-    
-    while (T--){
-        int r;
-        cin >> r;
-        
-        vector <int> s(r);
-        for (int i = 0; i < r; ++i){
-            cin >> s[i];
-        }
-        
-        sort(s.begin(), s.end());
-        
-        int median = s[r/2];
-        int result = 0;
-        
-        for (int i = 0; i < r; ++i){
-            result += abs(median - s[i]);
-        }
-        
-        cout << result << '\n';
-    }
-    return 0;
+int main ()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int i;
+	int n;
+	
+	cin >> n;
+	
+	while (n > 0) {
+		int r, home, t = 0;
+		
+		cin >> r;
+		
+		int s[r];
+		
+		for (i=0;i<r;i++)
+			cin >> s[i];
+			
+		sort(s, s + r);
+		
+		if (r % 2 == 1)
+			home = s[r/2];
+		else
+			home = (s[r/2-1] + s[r/2]) / 2;
+			
+		for (i=0;i<r;i++)
+			t += abs(home - s[i]);
+			
+		cout << t << endl;
+		
+		n--;
+	}
+
+	return 0;
 }
