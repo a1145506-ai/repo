da1d2a252a0201f5d45b79d3364f981be2ca6a31
a1145506-ai <a1145506@mail.nuk.edu.tw>
2026-06-02 01:40:36 +0000

    List of Conquests

---
+++ Problem/UVa_10420.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    map <string, int> mp;
+    string s;
+    getline(cin, s);
+    while (n--) {
+        getline(cin, s);
+        stringstream ss(s);
+        ss >> s;
+        mp[s]++;
+    }
+    for (auto i: mp) {
+        cout << i.first << " " << i.second << "\n";
+    }
+    return 0;
+}
